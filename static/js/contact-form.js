dcaca4627c706816e11b4837bb5104fc0d5fcb73
YQU2020 <yipeng.qu.20@ucl.ac.uk>
2022-03-15 09:58:40 +0000

    Add php file for contact form
    Add php code to make the form works

--- static/js/contact-form.js
+++ static/js/contact-form.js
@@ -1,16 +1,3 @@
-/*
---------------------------------
-Ajax Contact Form
---------------------------------
-+ https://github.com/pinceladasdaweb/Ajax-Contact-Form
-+ A Simple Ajax Contact Form developed in PHP with HTML5 Form validation.
-+ Has a fallback in jQuery for browsers that do not support HTML5 form validation.
-+ version 1.0.1
-+ Copyright 2014 Pedro Rogerio
-+ Licensed under the MIT license
-+ https://github.com/pinceladasdaweb/Ajax-Contact-Form
-*/
-
 (function ($, window, document, undefined) {
     'use strict';
 
