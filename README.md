10707914d7c84f51bdc910e82b33416f45dfdbee
YQU2020 <72596309+YQU2020@users.noreply.github.com>
2022-03-30 20:42:51 +0900

    adding formatting
    add the authors

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # CQDWebsite
 The code of the CQD report website.
+Authors: COMP0016-Team13-Yipeng Qu, Ahmad Daniel.
 
 WEBSITE URL: https://students.cs.ucl.ac.uk/2021/group13/
--- contact-form.php
+++ contact-form.php
@@ -1,4 +1,8 @@
 <?php
+/**
+ * Care Quality Dashboard Website
+ * Authors: COMP0016-Team13-Yipeng Qu
+ */
 // Configure Subject Prefix and Recipient
 $subjectPrefix = '[Contact via website]';
 $emailTo       = 'zcabyqu@ucl.ac.uk';
